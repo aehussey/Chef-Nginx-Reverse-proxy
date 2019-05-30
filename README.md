e83b38e19e354f15627963bc73c2f9e636d3e71f
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2019-05-30 14:42:00 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# nginx_reverse_proxy
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: nginx_reverse_proxy
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
